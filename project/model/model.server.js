bc7edeae03bb5807cb9fc6fa00c9e8d4bdfb500e
Tina Vivio <tinavivio@gmail.com>
2016-12-13 20:10:49 -0800

    working project

--- project/model/model.server.js
+++ project/model/model.server.js
@@ -2,7 +2,7 @@ module.exports = function() {
     var connectionString = 'mongodb://tvivio:jingo123@ds119728.mlab.com:19728/vivio_project_fall_2016';
 
     var mongoose = require('mongoose');
-    //mongoose.connect(connectionString);
+    mongoose.connect(connectionString);
 
     var userModel = require("./user/user.model.server.js")(mongoose);
     var messageModel = require("./message/message.model.server.js")(mongoose);
